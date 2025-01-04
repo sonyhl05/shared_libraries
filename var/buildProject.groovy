cfbbd09a906708d3ab8a12fbd2353f335df92130
sonyhl05 <sonyhl1918@gmail.com>
2025-01-04 22:03:37 +0530

    Update buildProject.groovy

--- var/buildProject.groovy
+++ var/buildProject.groovy
@@ -1,20 +1,24 @@
-def call() {
+def buildProject() {
     echo 'Building project with Maven...'
     sh 'mvn clean package'
 }
-def call() {
+
+def checkoutCode() {
     echo 'Checking out code...'
     checkout scm
 }
-def call() {
-    echo 'Cleaning up...'
+
+def cleanupProcesses() {
+    echo 'Cleaning up processes...'
     sh 'pkill -f "mvn spring-boot:run" || true'
 }
-def call() {
+
+def cleanupWorkspace() {
     echo 'Cleaning up the workspace...'
     deleteDir() // Deletes all files in the current workspace
 }
-def call() {
+
+def runSpringBootApp() {
     echo 'Running Spring Boot application...'
     sh 'nohup mvn spring-boot:run &'
     sleep(time: 15, unit: 'SECONDS')
@@ -22,33 +26,38 @@ def call() {
     def localIp = sh(script: "hostname -I | awk '{print \$1}'", returnStdout: true).trim()
     echo "The application is running and accessible at: http://${localIp}:8080"
 }
-def call() {
+
+def setupJava17() {
     echo 'Setting up Java 17...'
     sh 'sudo apt update'
     sh 'sudo apt install -y openjdk-17-jdk'
 }
-def call() {
+
+def setupMaven() {
     echo 'Setting up Maven...'
     sh 'sudo apt install -y maven'
 }
-def call() {
+
+def stopSpringBootApp() {
     echo 'Gracefully stopping the Spring Boot application...'
     sh 'mvn spring-boot:stop'
 }
-def call(String tagName, String message = 'Build tagging') {
+
+def tagBuild(String tagName, String message = 'Build tagging') {
     echo "Tagging the build with tag: ${tagName}"
     
-    // Tagging the current commit in Git
     sh """
         git tag -a ${tagName} -m '${message}'
         git push origin ${tagName}
     """
 }
-def call(String artifactPath) {
+
+def uploadArtifact(String artifactPath) {
     echo 'Uploading artifact...'
     archiveArtifacts artifacts: artifactPath, allowEmptyArchive: true
 }
-def call() {
+
+def validateAppRunning() {
     echo 'Validating that the app is running...'
     def response = sh(script: 'curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:8080', returnStdout: true).trim()
     if (response == "200") {
@@ -58,4 +67,3 @@ def call() {
         error("The app did not start correctly!")
     }
 }
-
